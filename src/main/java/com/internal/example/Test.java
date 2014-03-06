dcbd2f61caab4124abb3190e15e72a2f9753f515
CHAU <hoanghaichau85@gmail.com>
2014-03-06 21:40:50 +0700

    first version

---
+++ src/main/java/com/internal/example/Test.java
@@ -0,0 +1,13 @@
+package com.internal.example;
+
+import no.auke.util.DateTimeHelper;
+import no.auke.util.FileUtil;
+import no.auke.util.StringConv;
+
+public class Test {
+
+	public static void main(String[] args){
+		System.out.println("hello world");
+		
+	}
+}
